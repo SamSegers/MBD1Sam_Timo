77af3455b30f94f2da60ca808dbd3c11f954038f
vegatarier <sam.sam.s@hotmail.com>
2016-05-08 00:38:40 +0200

    andere bluetooth

--- www/js/exchange.js
+++ www/js/exchange.js
@@ -1,21 +1,3 @@
 $(function(){
-	window.bluetooth.isEnabled(isEnabledSuccess, isEnabledError);
-	enableBluetooth();
+
 });
-
-
-function checkBluetoothStatus() {
-        window.bluetooth.isEnabled(isEnabledSuccess, isEnabledError);
-    }
-
-function isEnabledSuccess(isEnabled){
-}
-
-function isEnabledError(error){
-
-}
-
-function enableBluetooth()
-{
-	window.bluetooth.enable(isEnabledSuccess, isEnabledError);
-}
