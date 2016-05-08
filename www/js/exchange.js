0de8f176834812b09f0e0810b3f72c042d3fa66a
vegatarier <sam.sam.s@hotmail.com>
2016-05-08 12:59:10 +0200

    bluetooth

--- www/js/exchange.js
+++ www/js/exchange.js
@@ -1,23 +1,13 @@
-
-function onError(reason) {
-        console.log("ERROR: " + reason);
-    }
-
-function onDiscoverDevice(device) {
-        /*var listItem = document.createElement('li'),
-            html = '<b>' + device.name + '</b><br/>' +
-                'RSSI: ' + device.rssi + '&nbsp;|&nbsp;' +
-                device.id;
-
-        listItem.dataset.deviceId = device.id;
-        listItem.innerHTML = html;
-        deviceList.appendChild(listItem);*/
-    }
-
+function failure(err)
+{
+	console.log(err);
+}
 
 $(function(){
-	consol.log("test1");
- ble.scan([], 5, onDiscoverDevice, onError);
- console.log("test2");
+	console.log("test1");
+ble.scan([], 5, function(device) {
+    console.log(JSON.stringify(device));
+}, failure);
+console.log("test2");
 });
 
