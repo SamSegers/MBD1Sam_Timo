6688ebcec01ab438ec0c458dc0b9e38a369bf1d7
Timo Hoff <timohoff@live.nl>
2016-05-08 23:04:10 +0200

    added loading icon

--- www/js/detail.js
+++ www/js/detail.js
@@ -3,19 +3,18 @@ $(document).on('pagebeforeshow', '#detail', function(){
 });
 
 function updateDetailPage(){
+	$('#detail > .loading').show();
 	$.getJSON("http://pokeapi.co/api/v2/pokemon/"+detailPokemonId, function(data){
+		$('#detail > .avatar').attr('src', "http://pokeapi.co/media/sprites/pokemon/"+detailPokemonId+".png");
 		$rows = $('#detail > .row');
-		$('#detail > img').attr('src', "http://pokeapi.co/media/sprites/pokemon/"+detailPokemonId+".png");
 		$rows.children('.name').text(data.name);
 		$rows.children('.species').text(data.species.name);
 		$rows.children('.base-experience').text(data.base_experience);
-		
 		var text = ''
 		for(var i=0; i<data.abilities.length; i++){
 			text += (i!=0?', ':'')+data.abilities[i].ability.name;
 		}
 		$rows.children('.abilities').text(text);
-
 		text = '';
 		for(var i=0; i<data.types.length; i++){
 			text += (i!=0?', ':'')+data.types[i].type.name;
@@ -23,5 +22,6 @@ function updateDetailPage(){
 		$rows.children('.types').text(text);
 		$rows.children('.height').text(data.height);
 		$rows.children('.weight').text(data.weight);
+		$('#detail > .loading').hide();
 	});
 }
