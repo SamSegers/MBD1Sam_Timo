f1b4ecc4a0a5d6b6579b6359b6b61e703933d81a
vegatarier <sam.sam.s@hotmail.com>
2016-05-08 11:47:28 +0200

    Revert "andere bluetooth"
    This reverts commit 77af3455b30f94f2da60ca808dbd3c11f954038f.

--- www/js/exchange.js
+++ www/js/exchange.js
@@ -1,3 +1,21 @@
 $(function(){
-
+	window.bluetooth.isEnabled(isEnabledSuccess, isEnabledError);
+	enableBluetooth();
 });
+
+
+function checkBluetoothStatus() {
+        window.bluetooth.isEnabled(isEnabledSuccess, isEnabledError);
+    }
+
+function isEnabledSuccess(isEnabled){
+}
+
+function isEnabledError(error){
+
+}
+
+function enableBluetooth()
+{
+	window.bluetooth.enable(isEnabledSuccess, isEnabledError);
+}
