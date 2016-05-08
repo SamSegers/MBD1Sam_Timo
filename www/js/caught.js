9c2d73045881eba05b6ecfb7d6aec55615407a83
Timo Hoff <timohoff@live.nl>
2016-05-09 00:30:48 +0200

    fixed caught list

--- www/js/caught.js
+++ www/js/caught.js
@@ -3,16 +3,17 @@ $(document).on('pagebeforeshow', '#caught', function(){
 });
 
 // click on item in list, go to detail page
-$("#detail").on("click", ".item", function(){
+$("#caught").on("click", ".item", function(){
 	detailPokemonId = $(this).attr('data-id');
 	$.mobile.navigate( "#detail", { transition : "slide", info: "info about the #detail hash" });
+});
 
 function loadCaughtPokemon(){
 	var html = '';
 	var odd = false;
+	console.log('hmm');
 	for(var i=0;i<caught.length;i++){
 		var pokemon = getPokemon(caught[i]);
-		console.log(pokemon);	
 		html += "<div class='item"+(odd?" odd":'')+"' data-id='"+pokemon.id+"'>";
 		html += "<img src='http://pokeapi.co/media/sprites/pokemon/"+pokemon.id+".png'/>";
 		html += "<span>"+pokemon.name+"</span>";
